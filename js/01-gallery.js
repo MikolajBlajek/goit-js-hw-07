4dd4f4c71123e65111d29d2ac2bc45b9c564551b
MikolajBlajek <128635948+MikolajBlajek@users.noreply.github.com>
2023-11-13 17:22:38 +0100

    HW-js-07 first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this line 
 
-console.log(galleryItems);
+const containerGalleryItems = document.querySelector("ul.gallery");  
+containerGalleryItems.addEventListener(`click`, (event) => {
+    event.preventDefault(); 
+       if (event.target.tagName === 'IMG') {
+        const source = event.target.dataset.source;
+        const lightbox = basicLightbox.create(`
+            <img src="${source}" />
+        `);
+        lightbox.show();
+    }
+});
+ 
+const images = galleryItems.map((item) => {
+    return `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+            />
+        </a>
+    </div>`;
+    });
+ 
+containerGalleryItems.insertAdjacentHTML(`beforeend`, images.join("")); 
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this line 
+const galleryContainer = document.querySelector("#gallery");
 
-console.log(galleryItems);
+  const galleryHTML = galleryItems.map(item => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>`;
+  }).join('');
+
+galleryContainer.innerHTML = galleryHTML; 
+  
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,        
+    captionDelay: 250,      
+    captionsData: 'alt',    
+    captionsPosition: 'bottom', 
+});
