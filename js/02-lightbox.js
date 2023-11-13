8aab1026a29ca96f4012abc829b3f1c7c63d07fb
MikolajBlajek <128635948+MikolajBlajek@users.noreply.github.com>
2023-11-13 21:44:56 +0100

    Corect hw07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,25 +1,28 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line 
 const galleryContainer = document.querySelector("#gallery");
 
-  const galleryHTML = galleryItems.map(item => {
-    return `<li class="gallery__item">
-        <a class="gallery__link" href="${item.original}">
-          <img
-            class="gallery__image"
-            src="${item.preview}"
-            data-source="${item.original}"
-            alt="${item.description}"
-          />
-        </a>
-      </li>`;
-  }).join('');
+const galleryHTML = galleryItems
+  .map(item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>`
+  )
+  .join('');
 
 galleryContainer.innerHTML = galleryHTML; 
   
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captions: true,        
-    captionDelay: 250,      
-    captionsData: 'alt',    
-    captionsPosition: 'bottom', 
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionsPosition: 'bottom',
 });
